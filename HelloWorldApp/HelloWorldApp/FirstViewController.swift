877c418ddf01079fe28542926a8c6059c092dc08
Dmytro Lavoryk <dlavoryk@gmail.com>
2019-04-02 23:02:22 +0300

    Created default 2 tabs app

---
+++ HelloWorldApp/HelloWorldApp/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  HelloWorldApp
+//
+//  Created by Dmytro Lavoryk on 4/1/19.
+//  Copyright © 2019 dlavoryk. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HelloWorldApp/HelloWorldApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  HelloWorldApp
+//
+//  Created by Dmytro Lavoryk on 4/1/19.
+//  Copyright © 2019 dlavoryk. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
